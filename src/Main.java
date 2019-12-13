44d3ee73b5877fb257d631bff9492e5a8812743f
kirillsmirnov1 <thegaldren@gmail.com>
2019-12-13 17:32:37 +0300

    check args

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        String path = "input.txt";
-
+        String path =
+                args.length > 0 && args[0].length() > 0
+                ? args[0]
+                : "input.txt";
 
         try(BufferedReader reader = new BufferedReader(new FileReader(path));
             BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
@@ -25,7 +27,7 @@ public class Main {
                 line = line.replaceAll(" — ", "\t");
 
                 if(!line.isEmpty()) {
-                    System.out.println(line);
+                    //System.out.println(line);
                     writer.write(line + "\r\n");
                 }
                 line = reader.readLine();
