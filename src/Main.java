6d375172910d412b0c4c8c4e162ca25f012c0664
kirillsmirnov1 <thegaldren@gmail.com>
2019-12-13 11:55:57 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String path = "input.txt";
+
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(path));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
+
+            String line = reader.readLine();
+            while (line != null){
+
+                line = line.replaceAll("[0-9]", "");
+
+                line = line.replaceAll("^noun", "");
+                line = line.replaceAll("^adj.", "");
+                line = line.replaceAll("^p. v.", "");
+                line = line.replaceAll("^phr.", "");
+                line = line.replaceAll("^adv.", "");
+                line = line.replaceAll("^verb", "");
+
+                line = line.replaceAll("нет перевода", "");
+
+                line = line.replaceAll(" — ", "\t");
+
+                if(!line.isEmpty()) {
+                    System.out.println(line);
+                    writer.write(line + "\r\n");
+                }
+                line = reader.readLine();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
